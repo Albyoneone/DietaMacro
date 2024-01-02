32c786ad6efe8d1e90bdc4e03bc1fc1cd2edc03d
Albyoneone <134053001+Albyoneone@users.noreply.github.com>
2024-01-02 18:40:34 +0100

    First commit

---
+++ app/src/main/java/com/example/dietamacro/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.dietamacro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.dietamacro.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.informazioni) {
+                replaceFragment(new InformazioniPersonaliFragment());
+            } else if (itemId == R.id.cibi) {
+                replaceFragment(new ListaCibiFragment());
+            }
+
+            return true;
+
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+      FragmentManager fragmentManager = getSupportFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+      fragmentTransaction.replace(R.id.frame_layout,fragment);
+      fragmentTransaction.commit();
+    }
+
+}
