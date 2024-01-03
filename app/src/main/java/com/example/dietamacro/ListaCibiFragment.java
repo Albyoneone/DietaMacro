7c0bf86032ad8e9d3389d8419e337fcfa9d734d8
Albyoneone <134053001+Albyoneone@users.noreply.github.com>
2024-01-03 19:22:59 +0100

    Aggiunta: - toolbar - creazione del fragment dettagli cibo - possibilità di schiacciare elementi della lista cibo e navigare in pagina dettaglio cibo

--- app/src/main/java/com/example/dietamacro/ListaCibiFragment.java
+++ app/src/main/java/com/example/dietamacro/ListaCibiFragment.java
@@ -1,5 +1,6 @@
 package com.example.dietamacro;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -24,6 +25,7 @@ public class ListaCibiFragment extends Fragment {
         return new ListaCibiFragment();
     }
 
+    String[] dati = {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10", "item11", "item12", "item13", "item14", "item15", "item16", "item17", "item18", "item19", "item20", "item21", "item22"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,14 +33,25 @@ public class ListaCibiFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_lista_cibi, container, false);
 
-        ListView listView = view.findViewById(R.id.listaCibo);
-
-        List<String> dati = Arrays.asList("Patate", "Cipolla", "Carote");
+        ListView lista = view.findViewById(R.id.listaCibo);
 
         ArrayAdapter<String> adapater = new ArrayAdapter<> (
                 requireContext(), android.R.layout.simple_list_item_1, dati);
 
-        listView.setAdapter(adapater);
+        lista.setAdapter(adapater);
+
+        lista.setOnItemClickListener((parent, view1, i, id) -> {
+            DettaglioCibo dettaglioFragment = new DettaglioCibo();
+            Bundle args = new Bundle();
+            args.putString("nome", dati[i]);
+            dettaglioFragment.setArguments(args);
+
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, dettaglioFragment)
+                .addToBackStack(null)
+                .commit();
+        });
+
         return view;
     }
 }
