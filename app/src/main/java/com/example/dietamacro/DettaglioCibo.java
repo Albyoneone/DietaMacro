90cd3532e070c5a2a2aee68dc0786a38801a9a76
Albyoneone <134053001+Albyoneone@users.noreply.github.com>
2024-01-10 17:11:01 +0100

    Aggiunta: - possibilità di tornare indietro dal fragment dettaglioCibo

--- app/src/main/java/com/example/dietamacro/DettaglioCibo.java
+++ app/src/main/java/com/example/dietamacro/DettaglioCibo.java
@@ -2,11 +2,14 @@ package com.example.dietamacro;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 public class DettaglioCibo extends Fragment {
 
@@ -23,8 +26,21 @@ public class DettaglioCibo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment_dettaglio_cibo, container, false);
 
 
-        return inflater.inflate(R.layout.fragment_dettaglio_cibo, container, false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(v -> {
+            //tasto per tornare indietro
+            getActivity().getSupportFragmentManager().popBackStack();
+        });
+
+
+
+        return view;
     }
 }
